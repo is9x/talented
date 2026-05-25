16f1b2f9d0b58b59401b707da38b365c759ede48
is9x <rlitb@outlook.com>
2026-05-25 16:14:46 -0500

    Fix error in popup

--- discordpopup.lua
+++ discordpopup.lua
@@ -1,24 +1,16 @@
 local link = "https://discord.gg/6WMEAm6FZK"
 
-game:GetService("StarterGui"):SetCore("SendNotification",{
-    Title = "Script loaded enjoy!",
-    Text = "creds to Put your name here",
-    Button1 = "Join the Discord",
-    Button2 = "Cancel",
-    Duration = 30
-})
-
--- Detect button press
-game:GetService("StarterGui"):SetCore("SendNotification",{
+game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Discord",
     Text = "Click Join to copy the link.",
     Button1 = "Join",
     Button2 = "Cancel",
     Duration = 10,
     Callback = function(button)
-        if button == "Button1" then
+        if button == "Join" then
             setclipboard(link)
-            game:GetService("StarterGui"):SetCore("SendNotification",{
+            
+            game:GetService("StarterGui"):SetCore("SendNotification", {
                 Title = "Link Copied!",
                 Text = "Discord invite copied to clipboard.",
                 Duration = 5
